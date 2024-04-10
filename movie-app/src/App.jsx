1c9efb6a64c6d9834924809f119e3c3268c2f44c
alex1451 <aleksprodolls@gmail.com>
2024-04-10 17:50:26 +0200

    first commit

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+// import Button from './components/Button/Button';
+// import CardButton from './components/CardButton/CardButton';
+// import JournalItem from './components/JournalItem/journalItem';
+
+function App() {
+	// const data = [
+	// 	{
+	// 		title: 'Подготовка к обновлению курсов',
+	// 		text: 'Горные походы открывают удивительные природные ландшафты, испытывают туристов физически и морально, дают возможность почувствовать себя первопроходцем',
+	// 		date: new Date()
+	// 	},
+	// 	{
+	// 		title: 'Поход в годы',
+	// 		text: 'Большое значение в горном туризме придается бытовому обустройству в походе, ведь все необходимое для жизнеобеспечения группы',
+	// 		date: new Date()
+	// 	}
+	// ];
+
+	return (
+		<>
+			<h1>Заголовок</h1>
+			{/* <p>Какой-то текст</p>
+			<Button />
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+			</CardButton>
+
+			<JournalItem
+				title={data[1].title}
+				text={data[1].text}
+				date={data[1].date}
+			/> */}
+		</ >
+	);
+}
+
+export default App;
